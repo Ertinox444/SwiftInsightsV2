8df8b8c46cb4dff88536e4428ba25a73742fd2b8
Ertinox444 <paulalexandre.pilon@gmail.com>
2024-03-12 10:55:31 +0100

    Ajoutez des fichiers projet.

---
+++ mainForm.cs
@@ -0,0 +1,10 @@
+namespace SwiftInsightsV2
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ menuForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwiftInsightsV2
+{
+    public partial class menuForm : Form
+    {
+        public menuForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
